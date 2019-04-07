df6dce8ee3db69454a8b017d63dfe69a8cb74333
Bogdan Acar <bogdan.acar95@gmail.com>
2019-04-07 23:10:58 +0300

    add eval metrics

--- clf.py
+++ clf.py
@@ -7,8 +7,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.svm import SVC
+from sklearn.svm import SVC, LinearSVC
 from sklearn.pipeline import Pipeline
+from sklearn.neighbors import KNeighborsClassifier
 
 stop_words = set(stopwords.words('english'))
 
@@ -22,13 +23,19 @@ def make_nb():
 	"""
 	nb_clf = Pipeline([('vect', CountVectorizer(stop_words = 'english')),
 					   ('tfidf', TfidfTransformer()),
-					   ('clf', OneVsRestClassifier(MultinomialNB()))])
+					   ('clf', OneVsRestClassifier(MultinomialNB(fit_prior = True, class_prior = None)))])
 	#nb_clf.set_params(clf__alpha = 0.01, tfidf__use_idf = True, vect__ngram_range = (1, 2))
 	return nb_clf
 
 def make_svm():
-	svm_clf = text_clf = Pipeline([('vect', CountVectorizer(stop_words = 'english')),
-		                           ('tfidf', TfidfTransformer()),
-     							   ('clf', OneVsRestClassifier(SVC(kernel = 'linear')))])
+	svm_clf = Pipeline([('vect', CountVectorizer(stop_words = 'english')),
+                        ('tfidf', TfidfTransformer()),
+   					   	('clf', OneVsRestClassifier(LinearSVC(), n_jobs = 1))])
 	#svm_clf.set_params(clf__alpha = 0.01, tfidf__use_idf = True, vect__ngram_range = (1, 1))
 	return svm_clf
+
+def make_knn():
+	knn_clf = Pipeline([('vect', CountVectorizer(stop_words = 'english')),
+                        ('tfidf', TfidfTransformer()),
+   					   	('clf', OneVsRestClassifier(KNeighborsClassifier(n_neighbors = 5)))])
+	return knn_clf
--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import xml.etree.ElementTree as ET
 from os import listdir
 from os.path import isfile, join
 import re
-import clf
+from clf import *
 import numpy as np
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support, confusion_matrix
+from sklearn import preprocessing
+lb = preprocessing.MultiLabelBinarizer()
+import itertools
+
 
 top_categories = ["acq", "corn", "crude", "earn", "grain", "interest", "money-fx", "ship", "trade", "wheat"]
 
@@ -17,16 +21,17 @@ def parse_xml(xml_file, train_data, train_targets, test_data, test_targets):
 	tree = ET.parse(xml_file)
 	root = tree.getroot()
 	for article in root.findall("REUTERS"):
-		if article.find("TEXT").find("BODY") != None and article.get("TOPICS") != None:
+		if article.find("TEXT").find("BODY") != None: #and article.get("TOPICS") != None:
 			text = article.find("TEXT").find("BODY").text
 			topics = [topic.text for topic in article.find("TOPICS").findall("D")]
-			labels = [1 if c in topics else 0 for c in top_categories]
-			if article.get("LEWISSPLIT") == "TRAIN":
-				train_data.append(text)
-				train_targets.append(labels)
-			elif article.get("LEWISSPLIT") == "TEST":
-				test_data.append(text)
-				test_targets.append(labels)
+			labels = [top_categories.index(topic) for topic in topics if topic in top_categories]
+			if labels != []:
+				if article.get("LEWISSPLIT") == "TRAIN":
+					train_data.append(text)
+					train_targets.append(labels)
+				elif article.get("LEWISSPLIT") == "TEST":
+					test_data.append(text)
+					test_targets.append(labels)
 
 def build_dataset(dataset_path):
 	dataset = read_dataset(dataset_path)
@@ -37,11 +42,14 @@ def build_dataset(dataset_path):
 
 def test_clf(clf):
 	train_data, train_targets, test_data, test_targets = build_dataset("reuters21578-xml")
-	for label, category in enumerate(top_categories):
-		clf.fit(train_data, train_targets)
-		predicted = clf.predict(test_data)
-		print(f"Test accuracy : {accuracy_score(test_targets[:, label], predicted)}")
+	train_targets = lb.fit_transform(train_targets)
+	clf.fit(train_data, train_targets)
+	predicted = clf.predict(test_data)
+	test_targets = lb.fit_transform(test_targets)
+	print(precision_recall_fscore_support(test_targets, predicted, average = "micro", warn_for = tuple()))
 
 if __name__ == '__main__':
-	test_clf(clf.make_nb())
-	test_clf(clf.make_svm())
+	print("SVM metrics")
+	test_clf(make_svm())
+	print("NB metrics")
+	test_clf(make_nb())
