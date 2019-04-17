5d97f75474c89b3bf4396b659e08900470c209c9
Bogdan Acar <bogdan.acar95@gmail.com>
2019-04-17 23:37:06 +0300

    add grid search

--- clf.py
+++ clf.py
@@ -1,45 +1,55 @@
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-import re
-import string
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.svm import SVC, LinearSVC
+from sklearn.svm import LinearSVC
 from sklearn.pipeline import Pipeline
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neighbors import NearestCentroid
+from sklearn.neighbors import KNeighborsClassifier, NearestCentroid
+from sklearn.model_selection import GridSearchCV
 
-stop_words = set(stopwords.words('english'))
+mn_nb_params = {
+	'vect__ngram_range': [(1, 1), (1, 2)],
+}
 
-def make_nb():
-	"""
-	Best parameters determined using grid search
-	Best score 0.829362
-	clf__alpha: 0.01
-	tfidf__use_idf: True
-	vect__ngram_range: (1, 2)
-	"""
-	nb_clf = Pipeline([('vect', CountVectorizer(stop_words = 'english')),
-					   ('tfidf', TfidfTransformer()),
+linear_svm_params = {
+	'vect__ngram_range': [(1, 1), (1, 2)],
+	'tfidf__use_idf': (True, False),
+	'clf__estimator__penalty': ('l1', 'l2'),
+	'clf__estimator__loss': ('hinge', 'squared_hinge'),
+}
+
+knn_params = {
+	#'vect__ngram_range': [(1, 1), (1, 2)],
+	#'tfidf__use_idf': (True, False),
+	'clf__estimator__n_neighbors': [3, 5, 7, 9],
+	#'clf__estimator__algorithm': ['ball_tree', 'kd_tree', 'auto']
+}
+
+def find_best_params(clf, parameters, train_data, train_targets):
+	gs_clf = GridSearchCV(clf, parameters, cv = 5, iid = False, n_jobs = -1)
+	gs_clf = gs_clf.fit(train_data, train_targets)
+	print("Best score %f" % gs_clf.best_score_)
+	for param_name in sorted(parameters.keys()):
+		print("%s: %r" % (param_name, gs_clf.best_params_[param_name]))
+
+def make_multinomial_nb():
+	mn_nb_clf = Pipeline([('vect', CountVectorizer(stop_words = 'english', ngram_range = (1 ,2))),
 					   ('clf', OneVsRestClassifier(MultinomialNB(fit_prior = True, class_prior = None)))])
-	#nb_clf.set_params(clf__alpha = 0.01, tfidf__use_idf = True, vect__ngram_range = (1, 2))
-	return OneVsRestClassifier(MultinomialNB(fit_prior = True, class_prior = None))
+	return mn_nb_clf
 
-def make_svm():
-	svm_clf = Pipeline([('vect', CountVectorizer(stop_words = 'english')),
+def make_linear_svm():
+	linear_svm_clf = Pipeline([('vect', CountVectorizer(stop_words = 'english')),
                         ('tfidf', TfidfTransformer()),
    					   	('clf', OneVsRestClassifier(LinearSVC(), n_jobs = 1))])
-	#svm_clf.set_params(clf__alpha = 0.01, tfidf__use_idf = True, vect__ngram_range = (1, 1))
-	return OneVsRestClassifier(LinearSVC(), n_jobs = 1)
+	return linear_svm_clf
 
 def make_knn():
 	knn_clf = Pipeline([('vect', CountVectorizer(stop_words = 'english')),
                         ('tfidf', TfidfTransformer()),
-   					   	('clf', OneVsRestClassifier(KNeighborsClassifier(n_neighbors = 5)))])
-	return OneVsRestClassifier(KNeighborsClassifier(n_neighbors = 5))
+   					   	('clf', OneVsRestClassifier(KNeighborsClassifier(n_neighbors = 7, n_jobs = 1)))])
+	return knn_clf
 
 def make_rocchio():
 	return OneVsRestClassifier(NearestCentroid())
+
+if __name__ == '__main__':
+	pass
--- main.py
+++ main.py
@@ -4,22 +4,11 @@ from os.path import isfile, join
 import re
 from clf import *
 import numpy as np
-from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 from sklearn import preprocessing
-lb = preprocessing.MultiLabelBinarizer()
-import itertools
-from imblearn.over_sampling import RandomOverSampler
-from imblearn.under_sampling import RandomUnderSampler
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.pipeline import Pipeline
-rs = RandomUnderSampler(random_state=0)
-from collections import Counter
 from pandas import DataFrame
 
-
-#top_categories = ["acq", "corn", "crude", "earn", "grain", "interest", "money-fx", "ship", "trade", "wheat"]
+lb = preprocessing.MultiLabelBinarizer()
 top_categories = ["earn", "acq", "money-fx", "grain", "crude", "trade", "interest", "ship", "wheat", "corn"]
 
 def read_dataset(dataset_path):
@@ -47,18 +36,13 @@ def build_dataset(dataset_path):
 	train_data, train_targets, test_data, test_targets = [], [], [], []
 	for xml_file in dataset:
 		parse_xml(xml_file, train_data, train_targets, test_data, test_targets)
-	###
-	pipeline = Pipeline([('vect', CountVectorizer(stop_words = 'english')),('tfidf', TfidfTransformer())])
-	train_data = pipeline.fit_transform(train_data)
 	train_targets = lb.fit_transform(train_targets)
-	test_data = pipeline.transform(test_data)
 	test_targets = lb.transform(test_targets)
-	###
 	return train_data, np.array(train_targets), test_data, np.array(test_targets)
 
 def test_models(clfs, train_data, train_targets, test_data, test_targets):
 	metrics = {
-		clf.estimator.__class__.__name__: {
+		clf.named_steps['clf'].estimator.__class__.__name__: {
 	        "precision": [],
 	        "recall": [],
 	        "fscore": []
@@ -70,11 +54,10 @@ def test_models(clfs, train_data, train_targets, test_data, test_targets):
 		predicted = clf.predict(test_data)
 		precision, recall, fscore, _ =  precision_recall_fscore_support(test_targets, predicted, average = None, warn_for = tuple())
 		micro_avg =  precision_recall_fscore_support(test_targets, predicted, average = "micro", warn_for = tuple())
-		#print(micro_avg)
-		metrics[clf.estimator.__class__.__name__]["precision"] = precision
-		metrics[clf.estimator.__class__.__name__]["recall"] = recall
-		metrics[clf.estimator.__class__.__name__]["fscore"] = fscore
-		metrics[clf.estimator.__class__.__name__]["micro_avg"] = micro_avg
+		metrics[clf.named_steps['clf'].estimator.__class__.__name__]["precision"] = precision
+		metrics[clf.named_steps['clf'].estimator.__class__.__name__]["recall"] = recall
+		metrics[clf.named_steps['clf'].estimator.__class__.__name__]["fscore"] = fscore
+		metrics[clf.named_steps['clf'].estimator.__class__.__name__]["micro_avg"] = micro_avg
 	return metrics
 
 def display_metrics(metrics):
@@ -88,32 +71,12 @@ def display_metrics(metrics):
 	fscores = DataFrame(fscores).append(micro_avg)
 	print(fscores)
 
-
-
-def test_clf(clf, train_data, train_targets, test_data, test_targets):
-	#train_data, train_targets = rs.fit_resample(train_data, train_targets)
-	#print(train_targets.shape)
-	clf.fit(train_data, train_targets)
-	predicted = clf.predict(test_data)
-	#test_targets = lb.fit_transform(test_targets)
-	precision, recall, fscore, support =  precision_recall_fscore_support(test_targets, predicted, average = None, warn_for = tuple())
-	print(f"class precision: {[float('%.2f' % p) for p in precision]}")
-	print(f"class recall   : {[float('%.2f' % p) for p in recall]}")
-	print(f"class fscore   : {[float('%.2f' % p) for p in fscore]}")
-	print("micro-avg ", precision_recall_fscore_support(test_targets, predicted, average = "micro", warn_for = tuple()))
-	print(support)
-
 if __name__ == '__main__':
-	models = [make_nb(), make_knn(), make_svm()]
 	train_data, train_targets, test_data, test_targets = build_dataset("reuters21578-xml")
+	# clf = make_knn() # make_linear_svm()
+	# print(clf.named_steps['clf'].estimator.get_params().keys())
+	# find_best_params(clf, knn_params, train_data, train_targets)
+
+	models = [make_multinomial_nb(), make_linear_svm(), make_knn()]
 	metrics = test_models(models, train_data, train_targets, test_data, test_targets)
 	display_metrics(metrics)
-	# train_data, train_targets, test_data, test_targets = build_dataset("reuters21578-xml")
-	# for i in range(10):
-	# 	print(top_categories[i], sum(train_targets[:, i]))
-	# print("SVM metrics")
-	# test_clf(make_svm(), train_data, train_targets, test_data, test_targets)
-	# print("NB metrics")
-	# test_clf(make_nb(), train_data, train_targets, test_data, test_targets)
-	# print("KNN metrics")
-	# test_clf(make_knn(), train_data, train_targets, test_data, test_targets)
